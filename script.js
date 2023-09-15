abdc27383b5d0acfaf66397f3b31165e60eed9e2
Meghanajakkidi <144362663+Meghanajakkidi@users.noreply.github.com>
2023-09-15 12:40:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,54 @@
+window.alert("hello guys welcome to the page");
+// this will show alert box//
+//variables//
+let a = 20;
+let b = 30;
+console.log(a);
+console.log(b);
+let c = a+b;
+console.log(c);
+let marksOfAPerson=40;//camelcase variables//
+//costants//
+const numbers= 35;
+console.log(numbers);
+//arthematic operators +,-,*,%,/ //
+let sample1 =10;
+let sample2 =30;
+console.log(sample1+sample2);
+console.log(sample1-sample2);
+console.log(sample1*sample2);
+console.log(sample1/sample2);
+console.log(sample1%sample2);
+//assignment operators =,+=,-=,*=,%= //
+let sample =10;
+console.log(sample += 2);
+console.log(sample -= 5);
+console.log(sample *= 15);
+console.log(sample %= 2);
+//comprision operator  ==,!=,>,<,<=,>= //
+let number1=15;
+let number2=25;
+console.log(number1==number2);
+console.log(number1!=number2);
+console.log(number1>=number2);
+console.log(number1<=number2);
+//string methods //
+let firstname ="full stack";
+let lastname ="development";
+console.log(firstname.length);
+console.log(lastname.slice());
+console.log(firstname.substring());
+console.log(lastname.replace())
+console.log(firstname.toUpperCase());
+console.log(lastname.toLowerCase());
+console.log(firstname.concat(lastname));
+console.log(firstname.trim());
+console.log(lastname.trimEnd());
+console.log(firstname.split());
+let demo1=22; //number datatype//
+let demo2="44"; //string datatype//
+console.log(demo1);
+console.log(demo2);
+console.log(demo1===demo2);
+
+
